d5e0847e21576c14bbb7e640b5624651199db754
sparrow3kk <sparrowekk@protonmail.com>
2022-05-23 22:48:46 +0200

    first challenge selector try out

---
+++ src/challenge_selector.py
@@ -0,0 +1,24 @@
+import random
+
+
+items_dictionary = {
+    'easy': [
+        {
+            'weapons': [
+                'elvish bow'
+            ], 
+            'armors': [
+                'paladin armor'
+            ],
+            'legs': [
+                'zaoan legs'
+            ],
+            'helmets': [
+                'zaoan helmet'
+            ]
+        }
+    ]
+}
+
+
+print(items_dictionary['easy'][0]['weapons'])

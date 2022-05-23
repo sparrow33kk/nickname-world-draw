a510d56be62e897eb5ff90214fa97d3d0ebf238b
sparrow3kk <sparrowekk@protonmail.com>
2022-05-23 22:57:04 +0200

    make items json

---
+++ src/bin/items.py
@@ -0,0 +1,18 @@
+items_dictionary = {
+    'easy': [
+        {
+            'weapons': [
+                'elvish bow'
+            ], 
+            'armors': [
+                'paladin armor'
+            ],
+            'legs': [
+                'zaoan legs'
+            ],
+            'helmets': [
+                'zaoan helmet'
+            ]
+        }
+    ]
+}

6e4ca31ce949a738eb1d0ffc843b1190e1ad6bd6
sparrow3kk <sparrowekk@protonmail.com>
2022-05-24 21:49:40 +0200

    make first challengers record

---
+++ src/bin/challenges.py
@@ -0,0 +1,17 @@
+challenges = {
+    'easy': [
+        {
+            '1' : [
+                {
+                    'Become a little hero': 'Finish all downport quests'
+                }, 
+                {
+                    ''
+                },
+                {
+
+                },
+            ]
+        }
+    ]
+}
